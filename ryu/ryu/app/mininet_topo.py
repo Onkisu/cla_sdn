9c52bfcf5ff62782e3d0d49825566ba53cce51c2
Onkisu <oniisasaki@gmail.com>
2025-09-02 11:16:18 +0700

    fix_topos

--- ryu/ryu/app/mininet_topo.py
+++ ryu/ryu/app/mininet_topo.py
@@ -1,36 +1,49 @@
 #!/usr/bin/python3
 from mininet.net import Mininet
-from mininet.node import RemoteController, OVSSwitch
+from mininet.node import RemoteController, OVSSwitch, Node
 from mininet.link import TCLink
 from mininet.topo import Topo
 from mininet.cli import CLI
 
+class LinuxRouter(Node):
+    def config(self, **params):
+        super(LinuxRouter, self).config(**params)
+        self.cmd("sysctl -w net.ipv4.ip_forward=1")
+
+    def terminate(self):
+        self.cmd("sysctl -w net.ipv4.ip_forward=0")
+        super(LinuxRouter, self).terminate()
+
 class ComplexTopo(Topo):
     def build(self):
+        # Router
+        r1 = self.addNode('r1', cls=LinuxRouter, ip='10.0.0.254/24')
+
+        # Switch
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
         s3 = self.addSwitch('s3')
 
-        # Subnet 10.0.0.0/24 (gw 10.0.0.254)
+        # Hosts
         h1 = self.addHost('h1', ip='10.0.0.1/24', defaultRoute='via 10.0.0.254')
         h2 = self.addHost('h2', ip='10.0.0.2/24', defaultRoute='via 10.0.0.254')
         h3 = self.addHost('h3', ip='10.0.0.3/24', defaultRoute='via 10.0.0.254')
 
-        # Subnet 10.0.1.0/24 (gw 10.0.1.254)
         h4 = self.addHost('h4', ip='10.0.1.1/24', defaultRoute='via 10.0.1.254')
         h5 = self.addHost('h5', ip='10.0.1.2/24', defaultRoute='via 10.0.1.254')
         h6 = self.addHost('h6', ip='10.0.1.3/24', defaultRoute='via 10.0.1.254')
 
-        # Subnet 10.0.2.0/24 (gw 10.0.2.254)
         h7 = self.addHost('h7', ip='10.0.2.1/24', defaultRoute='via 10.0.2.254')
 
+        # Link hosts ke switch
         self.addLink(h1, s1); self.addLink(h2, s1); self.addLink(h3, s1)
         self.addLink(h4, s2); self.addLink(h5, s2); self.addLink(h6, s2)
         self.addLink(h7, s3)
 
-        # Backbone
-        self.addLink(s1, s3)
-        self.addLink(s2, s3)
+        # Link router ke masing-masing switch (gateway tiap subnet)
+        self.addLink(r1, s1, intfName1='r1-eth1', params1={'ip': '10.0.0.254/24'})
+        self.addLink(r1, s2, intfName1='r1-eth2', params1={'ip': '10.0.1.254/24'})
+        self.addLink(r1, s3, intfName1='r1-eth3', params1={'ip': '10.0.2.254/24'})
 
 if __name__=="__main__":
     net = Mininet(topo=ComplexTopo(),
