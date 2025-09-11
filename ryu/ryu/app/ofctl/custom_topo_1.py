78313da081d2e4715ddb03dd687f8858aac99a7e
Onkisu <oniisasaki@gmail.com>
2025-09-12 01:28:56 +0700

    update forecast by combine user& app_v11k

--- ryu/ryu/app/ofctl/custom_topo_1.py
+++ ryu/ryu/app/ofctl/custom_topo_1.py
@@ -3,15 +3,13 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
-from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet, ether_types, ipv4, arp
+from ryu.lib.packet import packet, ethernet, ether_types
 
-
-class CollectorFriendlyController(app_manager.RyuApp):
+class SimpleSwitch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(CollectorFriendlyController, self).__init__(*args, **kwargs)
+        super(SimpleSwitch, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
@@ -19,29 +17,20 @@ class CollectorFriendlyController(app_manager.RyuApp):
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-
-        # Default flow: send unmatched packets to controller
         match = parser.OFPMatch()
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
 
-    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+    def add_flow(self, datapath, priority, match, actions):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                             actions)]
-        if buffer_id:
-            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
-                                    priority=priority, match=match,
-                                    instructions=inst)
-        else:
-            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                    match=match, instructions=inst)
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match, instructions=inst)
         datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
-    def _packet_in_handler(self, ev):
+    def packet_in_handler(self, ev):
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
@@ -49,69 +38,18 @@ class CollectorFriendlyController(app_manager.RyuApp):
         in_port = msg.match['in_port']
 
         pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocols(ethernet.ethernet)[0]
-
+        eth = pkt.get_protocol(ethernet.ethernet)
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            return  # ignore LLDP
+            return
 
-        dst = eth.dst
-        src = eth.src
         dpid = format(datapath.id, "d").zfill(16)
         self.mac_to_port.setdefault(dpid, {})
-
-        # Learn MAC
-        self.mac_to_port[dpid][src] = in_port
-
-        # Determine output port
-        out_port = self.mac_to_port[dpid].get(dst, ofproto.OFPP_FLOOD)
+        self.mac_to_port[dpid][eth.src] = in_port
+        out_port = self.mac_to_port[dpid].get(eth.dst, ofproto.OFPP_FLOOD)
 
         actions = [parser.OFPActionOutput(out_port)]
+        data = msg.data if msg.buffer_id == ofproto.OFP_NO_BUFFER else None
 
-        # Process ARP packets
-        arp_pkt = pkt.get_protocol(arp.arp)
-        if arp_pkt:
-            match = parser.OFPMatch(
-                eth_type=0x0806,
-                arp_spa=arp_pkt.src_ip,
-                arp_tpa=arp_pkt.dst_ip,
-                eth_src=src,
-                eth_dst=dst
-            )
-            self.add_flow(datapath, 1, match, actions)
-        else:
-            # Process IPv4 packets
-            ip_pkt = pkt.get_protocol(ipv4.ipv4)
-            if ip_pkt:
-                # FIX: Remove in_port from match to allow bidirectional traffic flow.
-                # A rule specific to the in_port will not match return packets.
-                match = parser.OFPMatch(
-                    eth_type=0x0800,
-                    eth_src=src,
-                    eth_dst=dst,
-                    ipv4_src=ip_pkt.src,
-                    ipv4_dst=ip_pkt.dst,
-                    ip_proto=ip_pkt.proto
-                )
-                self.add_flow(datapath, 1, match, actions)
-            else:
-                # Non-IP, fallback to L2 only
-                match = parser.OFPMatch(
-                    in_port=in_port,
-                    eth_src=src,
-                    eth_dst=dst
-                )
-                self.add_flow(datapath, 1, match, actions)
-
-        # Send packet out
-        data = None
-        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-            data = msg.data
-
-        out = parser.OFPPacketOut(
-            datapath=datapath,
-            buffer_id=msg.buffer_id,
-            in_port=in_port,
-            actions=actions,
-            data=data
-        )
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
         datapath.send_msg(out)
