0dfb20d45a95cd3bfe59471cb4c4bca042a3f235
Onkisu <oniisasaki@gmail.com>
2025-09-12 02:06:40 +0700

    update forecast by combine user& app_v11k

--- ryu/ryu/app/ofctl/custom_topo_1.py
+++ ryu/ryu/app/ofctl/custom_topo_1.py
@@ -3,13 +3,15 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
-from ryu.lib.packet import packet, ethernet, ether_types, ipv4, arp
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet, ether_types, ipv4, arp
 
-class L3RouterController(app_manager.RyuApp):
+
+class CollectorFriendlyController(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(L3RouterController, self).__init__(*args, **kwargs)
+        super(CollectorFriendlyController, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
@@ -18,6 +20,7 @@ class L3RouterController(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
+        # Default flow: send unmatched packets to controller
         match = parser.OFPMatch()
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
@@ -26,15 +29,15 @@ class L3RouterController(app_manager.RyuApp):
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
         if buffer_id:
             mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
-                                    priority=priority, match=match, instructions=inst)
-        else:
-            mod = parser.OFPFlowMod(datapath=datapath,
-                                    priority=priority,
-                                    match=match,
+                                    priority=priority, match=match,
                                     instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
         datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
@@ -46,45 +49,69 @@ class L3RouterController(app_manager.RyuApp):
         in_port = msg.match['in_port']
 
         pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocol(ethernet.ethernet)[0]
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
 
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            return
+            return  # ignore LLDP
 
         dst = eth.dst
         src = eth.src
-        dpid = str(datapath.id)
+        dpid = format(datapath.id, "d").zfill(16)
         self.mac_to_port.setdefault(dpid, {})
+
+        # Learn MAC
         self.mac_to_port[dpid][src] = in_port
 
+        # Determine output port
         out_port = self.mac_to_port[dpid].get(dst, ofproto.OFPP_FLOOD)
+
         actions = [parser.OFPActionOutput(out_port)]
 
+        # Process ARP packets
         arp_pkt = pkt.get_protocol(arp.arp)
         if arp_pkt:
-            match = parser.OFPMatch(eth_type=0x0806,
-                                    arp_spa=arp_pkt.src_ip,
-                                    arp_tpa=arp_pkt.dst_ip,
-                                    eth_src=src, eth_dst=dst)
+            match = parser.OFPMatch(
+                eth_type=0x0806,
+                arp_spa=arp_pkt.src_ip,
+                arp_tpa=arp_pkt.dst_ip,
+                eth_src=src,
+                eth_dst=dst
+            )
             self.add_flow(datapath, 1, match, actions)
         else:
+            # Process IPv4 packets
             ip_pkt = pkt.get_protocol(ipv4.ipv4)
             if ip_pkt:
+                # FIX: Remove in_port from match to allow bidirectional traffic flow.
+                # A rule specific to the in_port will not match return packets.
                 match = parser.OFPMatch(
                     eth_type=0x0800,
+                    eth_src=src,
+                    eth_dst=dst,
                     ipv4_src=ip_pkt.src,
                     ipv4_dst=ip_pkt.dst,
                     ip_proto=ip_pkt.proto
                 )
                 self.add_flow(datapath, 1, match, actions)
+            else:
+                # Non-IP, fallback to L2 only
+                match = parser.OFPMatch(
+                    in_port=in_port,
+                    eth_src=src,
+                    eth_dst=dst
+                )
+                self.add_flow(datapath, 1, match, actions)
 
+        # Send packet out
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
 
-        out = parser.OFPPacketOut(datapath=datapath,
-                                  buffer_id=msg.buffer_id,
-                                  in_port=in_port,
-                                  actions=actions,
-                                  data=data)
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=data
+        )
         datapath.send_msg(out)
