11b8ee27d403c329dcd10006a938168b6c249415
Onkisu <oniisasaki@gmail.com>
2025-09-12 02:28:30 +0700

    update forecast by combine user& app_vhmk

--- ryu/ryu/app/topo_automation_v2.py
+++ ryu/ryu/app/topo_automation_v2.py
@@ -10,23 +10,20 @@ class LinuxRouter(Node):
     def config(self, **params):
         super(LinuxRouter, self).config(**params)
         self.cmd("sysctl -w net.ipv4.ip_forward=1")
-
     def terminate(self):
         self.cmd("sysctl -w net.ipv4.ip_forward=0")
         super(LinuxRouter, self).terminate()
 
-class VPSReadyTopo(Topo):
+class InternetTopo(Topo):
     def build(self):
-        # Router
-        self.r1 = self.addNode('r1', cls=LinuxRouter)
+        r1 = self.addNode('r1', cls=LinuxRouter)
 
         # Switch internal
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
         s3 = self.addSwitch('s3')
-        s99 = self.addSwitch('s99')  # uplink switch
 
-        # Hosts internal
+        # Hosts
         h1 = self.addHost('h1', ip='10.0.0.1/24', defaultRoute='via 10.0.0.254')
         h2 = self.addHost('h2', ip='10.0.0.2/24', defaultRoute='via 10.0.0.254')
         h3 = self.addHost('h3', ip='10.0.0.3/24', defaultRoute='via 10.0.0.254')
@@ -35,75 +32,48 @@ class VPSReadyTopo(Topo):
         h6 = self.addHost('h6', ip='10.0.1.3/24', defaultRoute='via 10.0.1.254')
         h7 = self.addHost('h7', ip='10.0.2.1/24', defaultRoute='via 10.0.2.254')
 
-        # Link hosts ke switch internal
+        # Link hosts ke switch
         self.addLink(h1, s1); self.addLink(h2, s1); self.addLink(h3, s1)
         self.addLink(h4, s2); self.addLink(h5, s2); self.addLink(h6, s2)
         self.addLink(h7, s3)
 
-        # Link router ke internal switches
-        self.addLink(self.r1, s1, intfName1='r1-eth1', params1={'ip':'10.0.0.254/24'})
-        self.addLink(self.r1, s2, intfName1='r1-eth2', params1={'ip':'10.0.1.254/24'})
-        self.addLink(self.r1, s3, intfName1='r1-eth3', params1={'ip':'10.0.2.254/24'})
-
-        # Link router ke uplink VPS
-        self.addLink(self.r1, s99, intfName1='r1-eth0', params1={'ip':'192.168.100.1/24'})
-
-def setup_internet_nat(router):
-    """Konfigurasi NAT di router agar host internal bisa akses internet"""
-    intf = router.uplink_intf
-    gw   = router.uplink_gw
-
-    # Set default route di router
-    router.cmd(f"ip route add default via {gw} dev {intf}")
-
-    # MASQUERADE semua subnet internal
-    router.cmd(f"iptables -t nat -A POSTROUTING -s 10.0.0.0/16 -o {intf} -j MASQUERADE")
-
-    # Forward trafik untuk semua interface internal
-    for eth in ['r1-eth1','r1-eth2','r1-eth3']:
-        router.cmd(f"iptables -A FORWARD -i {intf} -o {eth} -m state --state ESTABLISHED,RELATED -j ACCEPT")
-        router.cmd(f"iptables -A FORWARD -o {intf} -i {eth} -j ACCEPT")
+        # Link router ke switch internal
+        self.addLink(r1, s1, intfName1='r1-eth1', params1={'ip':'10.0.0.254/24'})
+        self.addLink(r1, s2, intfName1='r1-eth2', params1={'ip':'10.0.1.254/24'})
+        self.addLink(r1, s3, intfName1='r1-eth3', params1={'ip':'10.0.2.254/24'})
 
 if __name__ == "__main__":
     setLogLevel("info")
 
-    # Buat network
-    topo = VPSReadyTopo()
     net = Mininet(
-        topo=topo,
+        topo=InternetTopo(),
         switch=OVSSwitch,
         controller=lambda name: RemoteController(name, ip="127.0.0.1", port=6633),
         link=TCLink
     )
     net.start()
 
-    # Ambil router
     r1 = net.get('r1')
-    # Set parameter uplink
-    r1.uplink_intf = 'ens3'
-    r1.uplink_gw   = '10.171.241.1'
 
-    # Setup NAT + internet
-    setup_internet_nat(r1)
+    # Pasang sisi r1 dari veth pair
+    r1.cmd("ip link set r1-to-vps netns %s" % r1.pid)
+    r1.cmd("ip addr add 192.168.200.2/24 dev r1-to-vps")
+    r1.cmd("ip link set r1-to-vps up")
+    r1.cmd("ip route add default via 192.168.200.1")
 
-    # Set DNS di semua host
+    # Set DNS host internal
     for hname in ("h1","h2","h3","h4","h5","h6","h7"):
         h = net.get(hname)
         h.cmd("bash -c 'echo \"nameserver 8.8.8.8\" > /etc/resolv.conf'")
         h.cmd("bash -c 'echo \"nameserver 8.8.4.4\" >> /etc/resolv.conf'")
 
-    # Mapping host -> IP router
+    # Test konektivitas
     router_ips = {
-        "h1": "10.0.0.254",
-        "h2": "10.0.0.254",
-        "h3": "10.0.0.254",
-        "h4": "10.0.1.254",
-        "h5": "10.0.1.254",
-        "h6": "10.0.1.254",
-        "h7": "10.0.2.254",
+        "h1":"10.0.0.254","h2":"10.0.0.254","h3":"10.0.0.254",
+        "h4":"10.0.1.254","h5":"10.0.1.254","h6":"10.0.1.254",
+        "h7":"10.0.2.254"
     }
 
-    # Test ping router & internet
     info("*** Testing connectivity:\n")
     for hname in router_ips:
         h = net.get(hname)
