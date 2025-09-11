f6c410619c1830a352490c7570570ec67dbb7eca
Onkisu <oniisasaki@gmail.com>
2025-09-12 03:00:42 +0700

    update forecast by combine user& app_vhmk

--- ryu/ryu/app/topo_automation_trial.py
+++ ryu/ryu/app/topo_automation_trial.py
@@ -1,52 +1,48 @@
+#!/usr/bin/python3
 from mininet.net import Mininet
 from mininet.node import RemoteController, Node
 from mininet.cli import CLI
 from mininet.link import TCLink
-from mininet.log import setLogLevel
-import os
+from mininet.log import setLogLevel, info
+import subprocess
 
 class LinuxRouter(Node):
+    """Router Linux dengan IP forwarding."""
     def config(self, **params):
-        super(LinuxRouter, self).config(**params)
-        os.system("sysctl -w net.ipv4.ip_forward=1")
+        super().config(**params)
+        self.cmd('sysctl -w net.ipv4.ip_forward=1')
 
 def run():
     net = Mininet(controller=RemoteController, link=TCLink)
 
-    c0 = net.addController('c0', ip='127.0.0.1', port=6653)
+    info('*** Adding controller\n')
+    c0 = net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6633)
 
-    # Hosts
-    hosts = [net.addHost(f'h{i}', ip=f'10.0.{i}.1/24') for i in range(1,4)]
+    info('*** Adding router\n')
+    r1 = net.addHost('r1', cls=LinuxRouter, ip='10.0.0.1/24')
 
-    # Switches
+    info('*** Adding switch\n')
     s1 = net.addSwitch('s1')
 
-    # Router
-    r1 = net.addHost('r1', cls=LinuxRouter, ip='10.0.0.254/24')
+    info('*** Adding hosts\n')
+    h1 = net.addHost('h1', ip='10.0.0.2/24', defaultRoute='via 10.0.0.1')
+    h2 = net.addHost('h2', ip='10.0.0.3/24', defaultRoute='via 10.0.0.1')
 
-    # Links
-    for h in hosts:
-        net.addLink(h, s1)
+    info('*** Creating links\n')
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
     net.addLink(r1, s1)
-
+    
+    info('*** Starting network\n')
     net.start()
 
-    # Set default routes
-    for i,h in enumerate(hosts, start=1):
-        h.cmd(f'ip route add default via 10.0.{i}.254')  # router IP sesuai subnet
+    # Setup NAT di router untuk koneksi internet
+    info('*** Configuring NAT\n')
+    r1.cmd('iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE')
+    r1.cmd('iptables -A FORWARD -i eth0 -o r1-eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT')
+    r1.cmd('iptables -A FORWARD -i r1-eth0 -o eth0 -j ACCEPT')
 
-    # Buat veth pair otomatis VPS <-> r1
-    os.system("ip link add veth-vps type veth peer name veth-r1")
-    os.system("ip link set veth-vps up")
-    os.system("ip addr add 192.168.200.1/24 dev veth-vps")  # VPS side
-
-    # Masukkan sisi r1 ke Mininet
-    r1.cmd("ip link set veth-r1 up")
-    r1.cmd("ip addr add 192.168.200.2/24 dev veth-r1")
-
-    # NAT supaya paket Mininet bisa keluar
-    os.system("iptables -t nat -A POSTROUTING -s 192.168.200.0/24 -o eth0 -j MASQUERADE")
-
+    info('*** Network ready\n')
     CLI(net)
     net.stop()
 
