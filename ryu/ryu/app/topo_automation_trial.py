c99a5424244a8a487d23ca52f5c63d3b68f24a38
Onkisu <oniisasaki@gmail.com>
2025-09-12 03:03:25 +0700

    update forecast by combine user& app_vhmk

--- ryu/ryu/app/topo_automation_trial.py
+++ ryu/ryu/app/topo_automation_trial.py
@@ -1,46 +1,36 @@
 #!/usr/bin/python3
 from mininet.net import Mininet
-from mininet.node import RemoteController, Node
-from mininet.cli import CLI
+from mininet.node import RemoteController, Node, NAT
 from mininet.link import TCLink
+from mininet.cli import CLI
 from mininet.log import setLogLevel, info
-import subprocess
-
-class LinuxRouter(Node):
-    """Router Linux dengan IP forwarding."""
-    def config(self, **params):
-        super().config(**params)
-        self.cmd('sysctl -w net.ipv4.ip_forward=1')
 
 def run():
     net = Mininet(controller=RemoteController, link=TCLink)
 
     info('*** Adding controller\n')
-    c0 = net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6633)
-
-    info('*** Adding router\n')
-    r1 = net.addHost('r1', cls=LinuxRouter, ip='10.0.0.1/24')
+    c0 = net.addController('c0', ip='127.0.0.1', port=6633)
 
     info('*** Adding switch\n')
     s1 = net.addSwitch('s1')
 
+    info('*** Adding NAT host\n')
+    nat0 = net.addHost('nat0', cls=NAT, ip='10.0.0.254/24', inetIntf='eth0')
+
     info('*** Adding hosts\n')
-    h1 = net.addHost('h1', ip='10.0.0.2/24', defaultRoute='via 10.0.0.1')
-    h2 = net.addHost('h2', ip='10.0.0.3/24', defaultRoute='via 10.0.0.1')
+    h1 = net.addHost('h1', ip='10.0.0.1/24', defaultRoute='via 10.0.0.254')
+    h2 = net.addHost('h2', ip='10.0.0.2/24', defaultRoute='via 10.0.0.254')
 
     info('*** Creating links\n')
     net.addLink(h1, s1)
     net.addLink(h2, s1)
-    net.addLink(r1, s1)
-    
+    net.addLink(nat0, s1)
+
     info('*** Starting network\n')
     net.start()
 
-    # Setup NAT di router untuk koneksi internet
     info('*** Configuring NAT\n')
-    r1.cmd('iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE')
-    r1.cmd('iptables -A FORWARD -i eth0 -o r1-eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT')
-    r1.cmd('iptables -A FORWARD -i r1-eth0 -o eth0 -j ACCEPT')
+    nat0.configDefault()  # otomatis setup NAT dan IP forwarding
 
     info('*** Network ready\n')
     CLI(net)
