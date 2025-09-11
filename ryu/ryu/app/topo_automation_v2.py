42bdd4f9de5b0f41348da75a20cb2be53e3357b8
Onkisu <oniisasaki@gmail.com>
2025-09-12 02:16:57 +0700

    update forecast by combine user& app_v12k

--- ryu/ryu/app/topo_automation_v2.py
+++ ryu/ryu/app/topo_automation_v2.py
@@ -5,28 +5,29 @@ from mininet.link import TCLink
 from mininet.topo import Topo
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
-import subprocess, time
 
 class LinuxRouter(Node):
     def config(self, **params):
         super(LinuxRouter, self).config(**params)
-        self.cmd("sysctl -w net.ipv4.ip_forward=1")  # enable forwarding
+        # Enable IP forwarding
+        self.cmd("sysctl -w net.ipv4.ip_forward=1")
 
     def terminate(self):
         self.cmd("sysctl -w net.ipv4.ip_forward=0")
         super(LinuxRouter, self).terminate()
 
-class InternetTopo(Topo):
-    def build(self):
+class VPSReadyTopo(Topo):
+    def build(self, uplink_intf='ens3', uplink_ip='192.168.100.1/24', uplink_gw='10.171.241.1'):
         # Router
         r1 = self.addNode('r1', cls=LinuxRouter)
 
-        # Switch internal
+        # Internal switches
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
         s3 = self.addSwitch('s3')
+        s99 = self.addSwitch('s99')  # switch untuk uplink
 
-        # Hosts internal
+        # Internal hosts
         h1 = self.addHost('h1', ip='10.0.0.1/24', defaultRoute='via 10.0.0.254')
         h2 = self.addHost('h2', ip='10.0.0.2/24', defaultRoute='via 10.0.0.254')
         h3 = self.addHost('h3', ip='10.0.0.3/24', defaultRoute='via 10.0.0.254')
@@ -35,50 +36,54 @@ class InternetTopo(Topo):
         h6 = self.addHost('h6', ip='10.0.1.3/24', defaultRoute='via 10.0.1.254')
         h7 = self.addHost('h7', ip='10.0.2.1/24', defaultRoute='via 10.0.2.254')
 
-        # Link hosts ke switch
+        # Link hosts ke switch internal
         self.addLink(h1, s1); self.addLink(h2, s1); self.addLink(h3, s1)
         self.addLink(h4, s2); self.addLink(h5, s2); self.addLink(h6, s2)
         self.addLink(h7, s3)
 
-        # Link router ke internal switches
+        # Link router ke internal switch
         self.addLink(r1, s1, intfName1='r1-eth1', params1={'ip':'10.0.0.254/24'})
         self.addLink(r1, s2, intfName1='r1-eth2', params1={'ip':'10.0.1.254/24'})
         self.addLink(r1, s3, intfName1='r1-eth3', params1={'ip':'10.0.2.254/24'})
 
-        # Link router ke internet (host OS)
-        # r1-eth0 akan mengambil IP otomatis dari host via DHCP / NAT host
-        self.addLink(r1, self.addSwitch('s99'), intfName1='r1-eth0')
+        # Link router ke uplink VPS
+        self.addLink(r1, s99, intfName1='r1-eth0', params1={'ip':uplink_ip})
+
+        # Simpan parameter uplink
+        r1.uplink_intf = uplink_intf
+        r1.uplink_gw = uplink_gw
 
 def setup_internet_nat(router):
     """
-    Atur NAT internet via iptables di r1
+    Konfigurasi NAT di r1 agar host internal bisa akses internet
     """
-    # ambil nama interface yang ke host
-    intf = "r1-eth0"
-    router.cmd("ip addr flush dev {}".format(intf))
-    router.cmd("dhclient {}".format(intf))  # ambil IP dari host OS
-    router.cmd("sysctl -w net.ipv4.ip_forward=1")
+    intf = router.uplink_intf
+    gw = router.uplink_gw
 
-    # MASQUERADE semua subnet internal ke interface ini
-    router.cmd("iptables -t nat -A POSTROUTING -s 10.0.0.0/16 -o {} -j MASQUERADE".format(intf))
-    router.cmd("iptables -A FORWARD -i {} -o r1-eth1 -m state --state ESTABLISHED,RELATED -j ACCEPT".format(intf))
-    router.cmd("iptables -A FORWARD -o {} -i r1-eth1 -j ACCEPT".format(intf))
-    router.cmd("iptables -A FORWARD -i {} -o r1-eth2 -m state --state ESTABLISHED,RELATED -j ACCEPT".format(intf))
-    router.cmd("iptables -A FORWARD -o {} -i r1-eth2 -j ACCEPT".format(intf))
-    router.cmd("iptables -A FORWARD -i {} -o r1-eth3 -m state --state ESTABLISHED,RELATED -j ACCEPT".format(intf))
-    router.cmd("iptables -A FORWARD -o {} -i r1-eth3 -j ACCEPT".format(intf))
+    # Set default route di router
+    router.cmd(f"ip route add default via {gw} dev {intf}")
+
+    # MASQUERADE semua subnet internal ke uplink
+    router.cmd(f"iptables -t nat -A POSTROUTING -s 10.0.0.0/16 -o {intf} -j MASQUERADE")
+
+    # Forward trafik untuk semua interface internal
+    for eth in ['r1-eth1','r1-eth2','r1-eth3']:
+        router.cmd(f"iptables -A FORWARD -i {intf} -o {eth} -m state --state ESTABLISHED,RELATED -j ACCEPT")
+        router.cmd(f"iptables -A FORWARD -o {intf} -i {eth} -j ACCEPT")
 
 if __name__ == "__main__":
     setLogLevel("info")
-    net = Mininet(topo=InternetTopo(),
-                  switch=OVSSwitch,
-                  controller=lambda name: RemoteController(name, ip="127.0.0.1", port=6633),
-                  link=TCLink)
+    net = Mininet(
+        topo=VPSReadyTopo(uplink_intf='ens3',
+                           uplink_ip='192.168.100.1/24',
+                           uplink_gw='10.171.241.1'),
+        switch=OVSSwitch,
+        controller=lambda name: RemoteController(name, ip="127.0.0.1", port=6633),
+        link=TCLink
+    )
     net.start()
 
     r1 = net.get('r1')
-
-    # Set NAT / internet
     setup_internet_nat(r1)
 
     # Set DNS di semua host
@@ -88,17 +93,13 @@ if __name__ == "__main__":
         h.cmd("bash -c 'echo \"nameserver 8.8.4.4\" >> /etc/resolv.conf'")
 
     # Test konektivitas internal & internet
-    info("*** Testing internal connectivity:\n")
+    info("*** Testing connectivity:\n")
     for hname in ("h1","h4","h7"):
         h = net.get(hname)
-        result = h.cmd("ping -c 2 {}".format(r1.IP(hname[-1])))  # ping ke router
-        info(result)
-
-    info("*** Testing internet connectivity:\n")
-    for hname in ("h1","h4","h7"):
-        h = net.get(hname)
-        result = h.cmd("ping -c 3 8.8.8.8")
-        info(result)
+        info(f"Ping router dari {hname}:\n")
+        info(h.cmd(f"ping -c 2 {r1.IP(hname[-1])}"))
+        info(f"Ping internet dari {hname}:\n")
+        info(h.cmd("ping -c 3 8.8.8.8"))
 
     CLI(net)
     net.stop()
