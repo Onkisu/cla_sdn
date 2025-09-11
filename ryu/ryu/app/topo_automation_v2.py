1b8871cbd92ca2d8dc3ac4e061c1ea40849de938
Onkisu <oniisasaki@gmail.com>
2025-09-12 02:03:17 +0700

    update forecast by combine user& app_v11k

--- ryu/ryu/app/topo_automation_v2.py
+++ ryu/ryu/app/topo_automation_v2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 from mininet.net import Mininet
-from mininet.node import RemoteController, OVSSwitch, Node
-from mininet.nodelib import NAT
+from mininet.node import RemoteController, OVSSwitch, Node, NAT
 from mininet.link import TCLink
 from mininet.topo import Topo
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
+import subprocess, threading, time
 
 class LinuxRouter(Node):
     def config(self, **params):
@@ -16,25 +16,23 @@ class LinuxRouter(Node):
         self.cmd("sysctl -w net.ipv4.ip_forward=0")
         super(LinuxRouter, self).terminate()
 
-class InternetTopo(Topo):
+class SimpleTopo(Topo):
     def build(self):
-        # Router internal
-        r1 = self.addNode('r1', cls=LinuxRouter, ip='192.168.100.1/24')
+        # Router
+        r1 = self.addNode('r1', cls=LinuxRouter)
 
-        # NAT ke interface VPS ens3
-        nat = self.addNode('nat0', cls=NAT, ip='192.168.100.254/24',
-                          inNamespace=False, inetIntf='ens3')
-
-        # Switch untuk NAT & router
+        # NAT bawaan Mininet
+        nat = self.addNode('nat0', cls=NAT, ip='192.168.100.254/24', inNamespace=False)
         s_nat = self.addSwitch('s99')
         self.addLink(nat, s_nat)
-        self.addLink(r1, s_nat, intfName1='r1-eth0', params1={'ip': '192.168.100.1/24'})
+        self.addLink(r1, s_nat, intfName1='r1-eth0', params1={'ip':'192.168.100.1/24'})
 
-        # Internal switches & hosts
+        # Switch internal
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
         s3 = self.addSwitch('s3')
 
+        # Hosts
         h1 = self.addHost('h1', ip='10.0.0.1/24', defaultRoute='via 10.0.0.254')
         h2 = self.addHost('h2', ip='10.0.0.2/24', defaultRoute='via 10.0.0.254')
         h3 = self.addHost('h3', ip='10.0.0.3/24', defaultRoute='via 10.0.0.254')
@@ -48,42 +46,55 @@ class InternetTopo(Topo):
         self.addLink(h4, s2); self.addLink(h5, s2); self.addLink(h6, s2)
         self.addLink(h7, s3)
 
-        # Router ke masing-masing subnet
-        self.addLink(r1, s1, intfName1='r1-eth1', params1={'ip': '10.0.0.254/24'})
-        self.addLink(r1, s2, intfName1='r1-eth2', params1={'ip': '10.0.1.254/24'})
-        self.addLink(r1, s3, intfName1='r1-eth3', params1={'ip': '10.0.2.254/24'})
+        # Link router ke internal switches
+        self.addLink(r1, s1, intfName1='r1-eth1', params1={'ip':'10.0.0.254/24'})
+        self.addLink(r1, s2, intfName1='r1-eth2', params1={'ip':'10.0.1.254/24'})
+        self.addLink(r1, s3, intfName1='r1-eth3', params1={'ip':'10.0.2.254/24'})
+
+def run_forecast_loop():
+    while True:
+        info("\n*** Running AI Forecast...\n")
+        subprocess.call(["sudo", "python3", "forecast.py"])
+        time.sleep(900)
 
 if __name__=="__main__":
     setLogLevel("info")
-    net = Mininet(
-        topo=InternetTopo(),
-        switch=OVSSwitch,
-        controller=lambda name: RemoteController(name, ip="127.0.0.1", port=6633),
-        link=TCLink
-    )
+    net = Mininet(topo=SimpleTopo(),
+                  switch=OVSSwitch,
+                  controller=lambda name: RemoteController(name, ip="127.0.0.1", port=6633),
+                  link=TCLink)
     net.start()
 
+    # Ambil node
     r1 = net.get('r1')
     nat = net.get('nat0')
 
-    # Default route router ke NAT
+    # Konfigurasi NAT bawaan
+    nat.configDefault()
+    
+    # Set default route router ke NAT
     r1.cmd("ip route add default via 192.168.100.254")
-
-    # Enable IP forwarding & MASQUERADE di NAT
-    nat.cmd("sysctl -w net.ipv4.ip_forward=1")
-    nat.cmd("iptables -t nat -A POSTROUTING -o ens3 -j MASQUERADE")
-
+    
     # Set DNS di semua host
     for hname in ("h1","h2","h3","h4","h5","h6","h7"):
         h = net.get(hname)
         h.cmd("bash -c 'echo \"nameserver 8.8.8.8\" > /etc/resolv.conf'")
         h.cmd("bash -c 'echo \"nameserver 8.8.4.4\" >> /etc/resolv.conf'")
 
-    # Tes koneksi internal & NAT
-    for hname in ("h1","h2","h3","h4","h5","h6","h7"):
-        h = net.get(hname)
-        info(f"*** Testing {hname} ping 8.8.8.8\n")
-        info(h.cmd("ping -c 2 8.8.8.8"))
+    # Test konektivitas internal
+    info("*** Testing internal connectivity:\n")
+    result = net.get('h1').cmd('ping -c 2 10.0.0.254')
+    info(result)
+    info("*** Testing NAT connectivity:\n")
+    result = r1.cmd('ping -c 2 192.168.100.254')
+    info(result)
+    info("*** Testing internet connectivity:\n")
+    result = net.get('h1').cmd('ping -c 3 8.8.8.8')
+    info(result)
+
+    # Mulai loop forecast jika perlu
+    # t = threading.Thread(target=run_forecast_loop, daemon=True)
+    # t.start()
 
     CLI(net)
     net.stop()
