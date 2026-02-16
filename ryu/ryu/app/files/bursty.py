7198935fd4b6f2f3fbc689065e511f448a5cdb15
onii <oniisasaki@gmail.com>
2026-02-16 16:07:36 +0700

    new sim3

--- ryu/ryu/app/files/bursty.py
+++ ryu/ryu/app/files/bursty.py
@@ -60,7 +60,7 @@ def send_tcp(rate, duration):
             "mnexec", "-a",
             pid,
             "ITGSend",
-            "-T", "TCP",
+            "-T", "TCPSS",
             "-rp", "9001",
             "-a", DST_IP,
             "-c", "160",
